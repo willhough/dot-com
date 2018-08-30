5b89e89c2b2ae207db70cbb1129a272b372575c8
Will Hough <whough@nexcess.net>
2018-08-30 09:30:28 -0400

    Uploaded Base Files
    Uploaded all files from local dev.

---
+++ resume.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Hello there, thanks for looking at my source code. -->
+    <meta charset="utf-8">
+    <meta name="google-site-verification" content="bG0n5ZGdbsnSboxwfe1b9xZJf1H6o9gqfU5Z_Ulia3M">
+    <meta name="robots" content="nofollow">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Will Hough's HTML Resume">
+    <meta name="keywords" content="Will Hough, Resume, Hire Me, Designer, Web Designer, Web Developer, Photographer, Nerd, Geek">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:400,700">
+    <title>Will Hough | Resume</title>
+    <style>
+      body {
+        font-family: 'Oswald', sans-serif;
+      }
+      .contact {
+        text-align: right;
+      }
+      h1 {
+        text-transform: uppercase;
+        border-bottom: 5px solid #000;
+        padding-bottom: 10px;
+        margin-top: 10px;
+      }
+      small {
+        font-size: 40%;
+        text-transform: lowercase;
+      }
+      h2 {
+        text-transform: uppercase;
+        margin-top: 40px;
+      }
+      h3, .contact {
+        color: rgb(233, 29, 99);
+      }
+      h3 {
+        margin-top: 40px;
+        margin-bottom: 0;
+      }
+      h4 {
+        font-style: italic;
+      }
+      ul {
+        list-style-type: circle;
+        padding-left: 20px;
+        -webkit-margin-before: 0;
+      }
+      .container {
+        position: relative;
+      }
+      a.btn {
+        position: absolute;
+        right: 15px;
+      }
+      footer {
+        border-top: 1px solid #ccc;
+      }
+    </style>
+  </head>
+  <body class="container">
+    <a href="/media/Will_Hough-Resume.pdf" role="button" class="btn btn-sm btn-outline-danger d-print-none">Download PDF</a>
+    <h1><a href="/" class="d-print-none">Will Hough</a><span class="d-none d-print-inline">Will Hough</span> <small class="d-none d-print-inline">willhough.com/resume</small></h1>
+    <div class="row">
+      <address class="col">
+        292 Farmer Street<br>
+        Plymouth, MI 48170
+      </address>
+      <div class="col text-center">
+        <div><a href="https://www.linkedin.com/in/willhough/"><i class="fab fa-linkedin-in fa-lg fa-fw"></i></a> <span class="d-none d-print-inline">linkedin.com/in/willhough/</span></div>
+        <div><a href="https://github.com/willhough"><i class="fab fa-github fa-lg fa-fw"></i></a> <span class="d-none d-print-inline">github.com/willhough</span></div>
+      </div>
+      <div class="contact col">
+        +1 (269) 760-1865<br>
+        will@willhough.com
+      </div>
+    </div>
+    <h2>Tech Stack</h2>
+    <div class="row">
+      <div class="col-6 col-md-3 col-lg-2">HTML5</div>
+      <div class="col-6 col-md-3 col-lg-2">CSS3</div>
+      <div class="col-6 col-md-3 col-lg-2">Bootstrap</div>
+      <div class="col-6 col-md-3 col-lg-2">UI \ UX Design</div>
+      <div class="col-6 col-md-3 col-lg-2">Ubuntu</div>
+      <div class="col-6 col-md-3 col-lg-2">Apache</div>
+      <div class="col-6 col-md-3 col-lg-2">Illustrator</div>
+      <div class="col-6 col-md-3 col-lg-2">Photoshop</div>
+      <div class="col-6 col-md-3 col-lg-2">InDesign</div>
+      <div class="col-6 col-md-3 col-lg-2">Graphic Design</div>
+      <div class="col-6 col-md-3 col-lg-2">macOS</div>
+      <div class="col-6 col-md-3 col-lg-2">CentOS</div>
+      <div class="col-6 col-md-3 col-lg-2">MySQL</div>
+      <div class="col-6 col-md-3 col-lg-2">SSH</div>
+      <div class="col-6 col-md-3 col-lg-2">MailChimp</div>
+      <div class="col-6 col-md-3 col-lg-2">Magento</div>
+      <div class="col-6 col-md-3 col-lg-2">Linux</div>
+      <div class="col-6 col-md-3 col-lg-2">Git</div>
+      <div class="col-6 col-md-3 col-lg-2">WordPress</div>
+      <div class="col-6 col-md-3 col-lg-2">Vim</div>
+      <div class="col-6 col-md-3 col-lg-2">Brand Identity</div>
+      <div class="col-6 col-md-3 col-lg-2">SEO</div>
+      <div class="col-6 col-md-3 col-lg-2">PHP</div>
+      <div class="col-6 col-md-3 col-lg-2">JavaScript</div>
+      <div class="col-6 col-md-3 col-lg-2">DNS</div>
+      <div class="col-6 col-md-3 col-lg-2">CLI</div>
+      <div class="col-6 col-md-3 col-lg-2">Photography</div>
+    </div>
+    <h2>Portfolio</h2>
+    <div class="row">
+      <div class="col">
+        <ul>
+          <li><a href="https://www.behance.net/will_hough">Behance</a><span class="d-none d-print-block">behance.net/will_hough</span></li>
+          <li><a href="http://boards.core77.com">Core77 Boards</a><span class="d-none d-print-block">boards.core77.com</span></li>
+          <li><a href="http://www.mindovermatterrecords.com/">Mind Over Matter Records</a><span class="d-none d-print-block">mindovermatterrecords.com</span></li>
+        </ul>
+      </div>
+      <div class="col">
+        <ul>
+          <li><a href="https://www.futurehosting.com/">Futurehosting.com</a><span class="d-none d-print-block">futurehosting.com</span></li>
+          <li><a href="https://phpdetroit.io">PHP Detroit</a><span class="d-none d-print-block">phpdetroit.io</span></li>
+        </ul>
+      </div>
+      <div class="col">
+        <ul>
+          <li><a href="https://www.nexcess.net">Nexcess.net</a><span class="d-none d-print-block">nexcess.net</span></li>
+          <li><a href="https://blog.nexcess.net">Nexcess.net Blog</a><span class="d-none d-print-block">blog.nexcess.net</span></li>
+        </ul>
+      </div>
+    </div>
+    <h2>Professional Summary</h2>
+    <ul>
+      <li>Seasoned Web Designer, Front End Developer and Graphic Designer</li>
+      <li>Adept and creative multitasker capable of working simultaneously on multiple projects</li>
+      <li>Maintaining brand awareness</li>
+      <li>An ability to establish rapport with clients, management, and fellow employees</li>
+      <li>Detail oriented and deadline driven</li>
+      <li>Excellent presentation, communication, and interpersonal skills both verbal and written</li>
+      <li>An aptitude to effectively work in a team environment, independently and in correlation with other department managers and employees</li>
+    </ul>
+    <h2>Professional Experience</h2>
+    <h3 class="mt-0">Nexcess.net LLC, Southfield, MI</h3>
+    <h4>Senior Webmaster / Team Lead / Senior Web Designer</h4>
+    <p>JUNE 2009 - DECEMBER 2017</p>
+    <ul>
+      <li>Created and maintained multiple corporate websites</li>
+      <li>Maintained brand identity</li>
+      <li>Created imagery for advertising and marketing including corporate signs</li>
+      <li>Managed a team of developers and designers</li>
+      <li>Coordinated with software developers to create and maintain both the internal and external web-based software</li>
+      <li>Designed business cards, corporate letterhead, numerous corporate documents</li>
+      <li>Generated access cards for secured facility entrance</li>
+      <li>Initiated the creation and design of company trade show/event materials such as backdrops and displays</li>
+      <li>Cultivated a multitude of WordPress blogs</li>
+      <li>Redesigned the corporate website from conception to final using Bootstrap v2, then performed update to Bootstrap v3 <i>www.nexcess.net</i></li>
+      <li>Synchronized the PHP and JavaScript development team to both redesign and reconfigure the corporate web-based backend management software <i>NocWorx</i></li>
+      <li>Worked in succession with the PHP and JavaScript development team to redesign the client portal <i>portal.nexcess.net</i></li>
+    </ul>
+    <h3>Will Hough Enterprises Inc, Plainwell, MI</h3>
+    <h4>President / Founder</h4>
+    <p>SEPTEMBER 2008 - JUNE 2009</p>
+    <ul>
+      <li>Web Design and Front End Development</li>
+      <li>Magento Administration and Development</li>
+      <li>Product Photography</li>
+      <li>Computer Repair and Troubleshooting</li>
+      <li>Home Networking</li>
+    </ul>
+    <h3>M &amp; S Service Co. Inc, Allegan, MI</h3>
+    <h4>Web Development and Design</h4>
+    <p>JUNE 2004 - SEPTEMBER 2008</p>
+    <ul>
+      <li>Web design and development</li>
+      <li>Product photography</li>
+      <li>Windows workstation maintenance</li>
+      <li>Network Administrator</li>
+    </ul>
+    <h2>Education</h2>
+    <h3 class="mt-0">Kalamazoo Valley Community College</h3>
+    <h4>Specialization in: HTML and CSS - Web Layout &amp; Design</h4>
+    <p>1999 - 2000, Kalamazoo, MI</p>
+    <footer class="d-print-none">
+      <p><em>Last updated: July 17<sup>th</sup> 2018</em></p>
+    </footer>
+  </body>
+</html>
